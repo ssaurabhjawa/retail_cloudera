82a9252d7e555fee28ad2060d5669936f8c11ff4
Jai_Dev <jai_dev@macs-Mac-mini.local>
2022-01-22 14:37:40 +0530

    example kafka message

--- read.py
+++ read.py
@@ -1,5 +1,6 @@
 from kafka.admin import KafkaAdminClient, NewTopic, KafkaProducer
 from pyspark.sql import SparkSession
+import json
 
 import getpass
 username = getpass.getuser()
@@ -12,6 +13,13 @@ admin_client = KafkaAdminClient(
 topicName = NewTopic(name="example_topic", num_partitions=2, replication_factor=3)
 admin_client.create_topics(new_topics=topicName, validate_only=False)
 
+filename='/user/saurabh/test_source/part-00000'
+f = open(filename)
+data = json.load(f)
+file.close()
+
+
+
 producer = KafkaProducer(bootstrap_servers ="cdp01.itversity.com:2181,cdp02.itversity.com:2181,cdp03.itversity.com")
 producer.send(topicName, data.encode('utf-8'))
 producer.flush()
@@ -20,7 +28,7 @@ spark = SparkSession. \
     builder. \
     config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1'). \
     config('spark.ui.port', '0'). \
-    config('spark.sql.warehouse.dir', f'/user/{username}/warehouse'). \
+    config('spark.sql.warehouse.dir', f'/user/saurabh/warehouse'). \
     enableHiveSupport(). \
     appName('Python - Kafka and Spark Integration'). \
     master('yarn'). \
