d8b48859fa0f94083287a0d43bf898585122dad6
Jai_Dev <jai_dev@macs-Mac-mini.local>
2022-01-23 17:36:38 +0530

    retail_topic_5

--- read.py
+++ read.py
@@ -12,18 +12,26 @@ admin_client = KafkaAdminClient(
     client_id='test'
 )
 
-retail_topic_list = []
-retail_topic_list.append(NewTopic(name="retail_topic", num_partitions=2, replication_factor=3))
-admin_client.create_topics(new_topics=retail_topic_list, validate_only=False)
+topic_list = []
+topic_list.append(NewTopic(name="retail_topic", num_partitions=1, replication_factor=1))
+admin_client.create_topics(new_topics=topic_list, validate_only=False)
+
+
+
+
+
 
 filename='/user/saurabh/test_source/part-00000'
 f = open(filename)
 data = json.load(f)
-file.close()
+f.close()
 
-producer = KafkaProducer(security_protocol="SSL", bootstrap_servers ="cdp01.itversity.com:2181,cdp02.itversity.com:2181,cdp03.itversity.com",api_version=(0,11,5))
-producer.send(retail_topic_list, data.encode('utf-8'))
-producer.flush()
+producer = KafkaProducer(bootstrap_servers =['cdp01.itversity.com:2181,cdp02.itversity.com:2181,cdp03.itversity.com'],
+                        value_serializer=lambda x:
+                        dumps(x).encode('utf-8'))
+for t in topic_list:
+    producer.send(t, data.encode('utf-8'))
+    producer.flush()
 
 spark = SparkSession. \
     builder. \
