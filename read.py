bbe6f5f2a78a28b3835b0ddd35a29e30fb7cee77
Jai_Dev <jai_dev@macs-Mac-mini.local>
2022-01-23 17:21:27 +0530

    retail_topic_2

--- read.py
+++ read.py
@@ -12,25 +12,17 @@ admin_client = KafkaAdminClient(
     client_id='test'
 )
 
-from kafka.admin import KafkaAdminClient, NewTopic
-
-
-admin_client = KafkaAdminClient(
-    bootstrap_servers="localhost:9092",
-    client_id='test'
-)
-
-retail_topic = []
-retail_topic.append(NewTopic(name="retail_topic_1", num_partitions=2, replication_factor=3))
-admin_client.create_topics(new_topics=retail_topic, validate_only=False)
+retail_topic_list = []
+retail_topic_list.append(NewTopic(name="retail_topic", num_partitions=2, replication_factor=3))
+admin_client.create_topics(new_topics=retail_topic_list, validate_only=False)
 
 filename='/user/saurabh/test_source/part-00000'
 f = open(filename)
 data = json.load(f)
 file.close()
 
-producer = KafkaProducer(security_protocol="SSL", bootstrap_servers ="cdp01.itversity.com:2181,cdp02.itversity.com:2181,cdp03.itversity.com")
-producer.send(topicName, data.encode('utf-8'))
+producer = KafkaProducer(security_protocol="SSL", bootstrap_servers ="cdp01.itversity.com:2181,cdp02.itversity.com:2181,cdp03.itversity.com",api_version=(0,11,5))
+producer.send(retail_topic_list, data.encode('utf-8'))
 producer.flush()
 
 spark = SparkSession. \
